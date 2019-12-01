78fc56a8bfb3fe3cade0340a2c55ac4b44c46ead
Alison Yutaka Inoue <alison.inoue@gmail.com>
2019-12-01 18:21:07 -0300

    adding list entries module

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,6 +1,7 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
 
 import { Category } from "./pages/categories/shared/category.model";
+import { Entry } from './pages/entries/shared/entry.model';
 
 export class InMemoryDatabase implements InMemoryDbService {
   createDb(){
@@ -12,6 +13,11 @@ export class InMemoryDatabase implements InMemoryDbService {
       { id: 5, name: "Freela", description: "Trabalhos como freelancer"}
     ];
 
-    return { categories }
+    const entries: Entry[] = [
+      { id: 1, name: "Gás de cozinha", categoryId: categories[0].id, category: categories[0], paid: true, date: "12/01/2019", amount: "15,02", type: "expense", description: "Pagamentos de Conta da Casa" } as Entry,
+      { id: 2, name: "Pagode", categoryId: categories[1].id, category: categories[1], paid: true, date: "27/11/2019", amount: "8300,02", type: "revenue", description: "Salário" } as Entry,
+    ];
+
+    return { categories, entries }
   }
 }
